80cb8dd4fa3a488d36b333a0808d128e920c97e0
gnanakemilton-i <gnanakemilton@gmail.com>
2023-04-12 14:50:29 +0530

    snack squad application

---
+++ SnackOrderingApp/app/src/main/java/com/example/snackorderingapp/OrderDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.snackorderingapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Order::class], version = 1)
+abstract
+class OrderDatabase : RoomDatabase() {
+
+    abstract fun orderDao(): OrderDao
+
+    companion object {
+
+        @Volatile
+        private var instance: OrderDatabase? = null
+
+        fun getDatabase(context: Context): OrderDatabase {
+            return instance ?: synchronized(this) {
+                val newInstance = Room.databaseBuilder(
+                    context.applicationContext,
+                    OrderDatabase::class.java,
+                    "order_database"
+                ).build()
+                instance = newInstance
+                newInstance
+            }
+        }
+    }
+}
